6d6c24d64d630ccc92002091e3cac802a398fb2b
Ayush Nagar <ayushnagar0018@gmail.com>
2024-09-29 19:47:57 +0530

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import pandas as pd
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -8,23 +9,39 @@ app = Flask(__name__)
 # Load the saved model
 model = joblib.load('models/best_model.pkl')
 
+feature_names = ['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges', 'gender_Male', 
+                 'Partner_Yes', 'Dependents_Yes', 'PhoneService_Yes', 'MultipleLines_No phone service',
+                 'MultipleLines_Yes', 'InternetService_Fiber optic', 'InternetService_No', 
+                 'OnlineSecurity_No internet service', 'OnlineSecurity_Yes', 'OnlineBackup_No internet service', 
+                 'OnlineBackup_Yes', 'DeviceProtection_No internet service', 'DeviceProtection_Yes', 
+                 'TechSupport_No internet service', 'TechSupport_Yes', 'StreamingTV_No internet service', 
+                 'StreamingTV_Yes', 'StreamingMovies_No internet service', 'StreamingMovies_Yes', 
+                 'Contract_One year', 'Contract_Two year', 'PaperlessBilling_Yes', 'PaymentMethod_Credit card (automatic)', 
+                 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check', 'BothStreamingServices', 'AvgMonthlyCharge']
+
 # Define the /predict route
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request (JSON format)
-    data = request.get_json()
+    try:
+        # Get JSON data from POST request
+        input_data = request.json
 
-    # Extract the features from the input JSON
-    features = np.array([data['features']])  # Ensure input is a 2D array
+        # Convert input data to a DataFrame using the correct feature names
+        input_df = pd.DataFrame([input_data], columns=feature_names)
 
-    # Make a prediction
-    prediction = model.predict(features)
-   
-    
-    # Return the prediction result as JSON
-    return jsonify({
-        'prediction': "Yes" if int(prediction[0]) else "No"
-    })
+        # Make prediction
+        prediction = model.predict(input_df)
+        print(prediction)
+        # Return prediction as JSON
+        return jsonify({
+            'prediction': int(prediction[0])
+        })
+
+    except Exception as e:
+        return jsonify({
+            'error': str(e)
+        })
 
 
 @app.route('/', methods=['GET'])
